40fa25619c220077115b2f285120d9f2d55f7b0f
Frederik Shull <zlyfer@pm.me>
2022-03-04 13:13:21 +0100

    draw dots, themes, auto light/dark theme

--- dot.js
+++ dot.js
@@ -3,7 +3,6 @@
 class Dot {
   constructor(x, y) {
     this.pos = createVector(x, y);
-    // this.vel = createVector(random(-1, 1), random(-1, 1));
     this.vel = createVector(0, fps < 120 ? 0.4 : 0.2);
     this.vel.rotate(random(-PI, PI));
   }
@@ -21,24 +20,29 @@ class Dot {
     if (this.pos.y < 5 || this.pos.y > height - 5) {
       this.vel.y *= -1;
     }
+    if (this.pos.x < 0 || this.pos.x > width) {
+      this.pos.x = random(5, width - 5);
+    }
+    if (this.pos.y < 0 || this.pos.y > height) {
+      this.pos.y = random(5, height - 5);
+    }
   }
 
   draw(dots) {
     // Draw dot:
     push();
     noFill();
-    strokeWeight(2);
-    stroke(255);
+    strokeWeight(3);
+    stroke(theme.dots);
     translate(this.pos.x, this.pos.y);
     point(0, 0);
     pop();
 
-    // Draw connection to nearby dots:
     dots.forEach((d) => {
-      if (d !== this && d.pos.dist(this.pos) < 150) {
+      if (d !== this && d.pos.dist(this.pos) < connectionDistance) {
         push();
-        strokeWeight(map(d.pos.dist(this.pos), 0, 150, 0.3, 0.05));
-        stroke(map(d.pos.dist(this.pos), 0, 50, 0, 150));
+        strokeWeight(map(d.pos.dist(this.pos), 0, 150, 0.5, 0.1));
+        stroke(map(d.pos.dist(this.pos), 0, 50, theme.linesMin, theme.linesMax));
         line(this.pos.x, this.pos.y, d.pos.x, d.pos.y);
         pop();
       }
--- sketch.js
+++ sketch.js
@@ -1,15 +1,60 @@
 // jshint esversion: 9
 
-let dots = [];
 const fps = 200;
+
+// settings:
+const drawArea = 100;
+const maxDraw = 8;
+const drawForce = 0.008;
+const connectionDistance = 150;
+
+// theme:
+const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+const themes = {
+  light: {
+    background: 255,
+    dots: 0,
+    linesMin: 0,
+    linesMax: 50,
+  },
+  dark: {
+    background: 0,
+    dots: 255,
+    linesMin: 50,
+    linesMax: 255,
+  },
+  mLight: {
+    background: 240,
+    dots: 22,
+    linesMin: 22,
+    linesMax: 72,
+  },
+  mDark: {
+    background: 22,
+    dots: 220,
+    linesMin: 220,
+    linesMax: 150,
+  },
+};
+var theme = mediaQuery.matches ? themes.mDark : themes.mLight;
+
+let dots = [];
 
 function preload() {}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < (windowWidth * windowHeight) / Math.pow(144, 2); i++) {
     dots.push(new Dot(random(5, width - 5), random(5, height - 5)));
   }
+
+  mediaQuery.addEventListener("change", (e) => {
+    if (e.matches) {
+      theme = themes.mDark;
+    } else {
+      theme = themes.mLight;
+    }
+  });
 }
 
 function windowResized() {
@@ -18,8 +63,15 @@ function windowResized() {
 
 function draw() {
   frameRate(fps);
-  background(22);
+  background(theme.background);
+  let inRange = 0;
   dots.forEach((d) => {
+    if (dist(d.pos.x, d.pos.y, mouseX, mouseY) < drawArea) {
+      inRange++;
+      if (mouseIsPressed && inRange < maxDraw) {
+        d.pos = p5.Vector.lerp(d.pos, createVector(mouseX, mouseY), drawForce);
+      }
+    }
     d.update(dots);
   });
 }
